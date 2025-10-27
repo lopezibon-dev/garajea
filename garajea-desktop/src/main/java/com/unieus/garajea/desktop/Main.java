695e21484c778f007892d7eeff5ec98fa8d50da6
Ibon Lopez <ibonlopezdev@gmail.com>
2025-10-27 18:24:07 +0100

    Maven egitura sortu

---
+++ garajea-desktop/src/main/java/com/unieus/garajea/desktop/Main.java
@@ -0,0 +1,7 @@
+package com.unieus.garajea.desktop;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ garajea-webapp/src/main/java/com/unieus/garajea/webapp/Main.java
@@ -0,0 +1,7 @@
+package com.unieus.garajea.webapp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
